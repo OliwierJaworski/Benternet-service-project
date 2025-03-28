fd80c3fc5b489ab7b09f1a7042c221d9bb65a8d1
oliwier <oliwier.jaworski@student.pxl.be>
2025-03-28 16:37:39 +0100

    #** (**) fixed non compatible benternet code

--- app/client/client.cpp
+++ app/client/client.cpp
@@ -1,34 +1,47 @@
-//  Hello World client
-#include <zmqpp/zmqpp.hpp>
 #include <string>
 #include <iostream>
+#include <zmq.hpp>
 
-using namespace std;
+int main(int argc, char* argv[]) {
+    // Controleer argumenten
+    if (argc < 3) {
+        std::cerr << "Gebruik: client.exe <topic> <gebruikersnaam>\n";
+        return 1;
+    }
 
-int main(int argc, char *argv[]) {
-  const string endpoint = "tcp://localhost:5555";
+    std::string topic = argv[1];
+    std::string naam = argv[2];
 
-  // initialize the 0MQ context
-  zmqpp::context context;
+    // Initialiseer ZMQ context
+    zmq::context_t context{1};
 
-  // generate a push socket
-  zmqpp::socket_type type = zmqpp::socket_type::req;
-  zmqpp::socket socket (context, type);
+    // PUSH socket om aanvragen te versturen
+    zmq::socket_t push_socket{context, zmq::socket_type::push};
+    push_socket.connect("tcp://benternet.pxl-ea-ict.be:24041");
 
-  // open the connection
-  cout << "Connecting to hello world server…" << endl;
-  socket.connect(endpoint);
-  int request_nbr;
-  for (request_nbr = 0; request_nbr != 10; request_nbr++) {
-    // send a message
-    cout << "Sending Hello " << request_nbr <<"…" << endl;
-    zmqpp::message message;
-    // compose a message from a string and a number
-    message << "Hello";
-    socket.send(message);
-    string buffer;
-    socket.receive(buffer);
-    
-    cout << "Received World " << request_nbr << endl;
-  }
+    // SUB socket om antwoorden te ontvangen
+    zmq::socket_t sub_socket{context, zmq::socket_type::sub};
+    sub_socket.connect("tcp://benternet.pxl-ea-ict.be:24042");
+
+    // Subscribe op antwoorden specifiek voor deze gebruiker en topic
+    std::string sub_topic = topic + "!>" + naam + ">";
+    sub_socket.setsockopt(ZMQ_SUBSCRIBE, sub_topic.c_str(), sub_topic.length());
+
+    // Verzend aanvraag via PUSH
+    std::string push_message = topic + "?>" + naam + ">";
+    std::cout << "Verzonden aanvraag: " << push_message << std::endl;
+    push_socket.send(zmq::buffer(push_message), zmq::send_flags::none);
+
+    // Luister naar antwoorden
+    zmq::message_t reply;
+    sub_socket.recv(reply, zmq::recv_flags::none);
+    std::string antwoord = reply.to_string();
+
+    std::cout << "Ontvangen antwoord: " << antwoord << std::endl;
+
+    // Houd venster open zodat antwoord leesbaar blijft
+    std::cout << "\nDruk ENTER om te sluiten...";
+    std::cin.get();
+
+    return 0;
 }
--- app/server/server.cpp
+++ app/server/server.cpp
@@ -1,36 +1,56 @@
-#include <iostream>
 #include <string>
-#include <chrono>
-#include <thread>
-#include "LLMInference.h"
-#include "zmqpp/zmqpp.hpp"
+#include <iostream>
+#include <random>
+#include <zmq.hpp>
 
-using namespace std;
-int main(int argc, char *argv[]) {
-    const string endpoint = "tcp://*:5555";
-  
-    // initialize the 0MQ context
-    zmqpp::context context;
-  
-    // generate a pull socket
-    zmqpp::socket_type type = zmqpp::socket_type::reply;
-    zmqpp::socket socket (context, type);
-  
-    // bind to the socket
-    socket.bind(endpoint);
-    while (1) {
-      // receive the message
-      zmqpp::message message;
-      // decompose the message 
-      socket.receive(message);
-      string text;
-      message >> text;
-  
-      //Do some 'work'
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-      cout << "Received Hello" << endl;
-      socket.send("World");
+int main() {
+    zmq::context_t context(1);
+
+    // SUB socket listens for weather requests
+    zmq::socket_t sub_socket(context, zmq::socket_type::sub);
+    sub_socket.connect("tcp://benternet.pxl-ea-ict.be:24042");
+
+    // PUSH socket sends answers
+    zmq::socket_t push_socket(context, zmq::socket_type::push);
+    push_socket.connect("tcp://benternet.pxl-ea-ict.be:24041");
+
+    // Subscribe to all "theweatheris?>" requests
+    std::string sub_topic = "theweatheris?>";
+    sub_socket.setsockopt(ZMQ_SUBSCRIBE, sub_topic.c_str(), sub_topic.length());
+
+    // Random weather generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::vector<std::string> weather_conditions = {
+        "sunny", "cloudy", "rainy", "stormy", "snowy", "foggy", "windy"
+    };
+    std::uniform_int_distribution<> distrib(0, weather_conditions.size() - 1);
+
+    std::cout << "Weather service active on Benternet..." << std::endl;
+
+    while (true) {
+        zmq::message_t request;
+        sub_socket.recv(request, zmq::recv_flags::none);
+
+        std::string received_msg = request.to_string();
+        std::cout << "Received request: " << received_msg << std::endl;
+
+        // Extract username from message
+        std::string user = received_msg.substr(sub_topic.length());
+
+        // Pick a random weather condition
+        std::string random_weather = weather_conditions[distrib(gen)];
+
+        // Construct the response
+        std::string response = "theweatheris!>" + user + ">" + random_weather;
+
+        // Send the response via PUSH socket
+        zmq::message_t msg(response.size());
+        memcpy(msg.data(), response.c_str(), response.size());
+
+        push_socket.send(msg, zmq::send_flags::none);
+        std::cout << "Sent response: " << response << std::endl;
     }
-  
-  }
 
+    return 0;
+}
