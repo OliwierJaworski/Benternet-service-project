fe40e9eb872af6312b25ddde569603aa151685ab
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2025-04-07 20:53:39 +0200

    #* (2) llm studio server response acquired

--- app/main.cpp
+++ app/main.cpp
@@ -3,7 +3,16 @@
 #include "external/openai-cpp/include/openai/openai.hpp"
 
 int main(int argc, char* argv[]) {
-    openai::start("mykey","myorg");
+    openai::start("lm-studio","",true,"http://127.0.0.1:1234/v1/");
+    auto chat = openai::chat().create(R"(
+        {
+            "model": "deepseek-r1-distill-qwen-7b",
+            "messages":[{"role":"user", "content":"play game"}],
+            "max_tokens": 1000,
+            "temperature": 0
+        }
+        )"_json);
+        std::cout << "Response is:\n" << chat.dump(2) << '\n'; 
     std::cout << "hello from main.cpp";
  
     return 0;
