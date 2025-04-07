4c3288e6d9438dc511e198c48d462a1fc7247a57
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2025-04-07 22:54:42 +0200

    #* (3) fixed thinking process being displayed for user

--- app/main.cpp
+++ app/main.cpp
@@ -2,18 +2,23 @@
 #include "openai.hpp"
 #include "external/openai-cpp/include/openai/openai.hpp"
 
+using namespace nlohmann;
 int main(int argc, char* argv[]) {
     openai::start("lm-studio","",true,"http://127.0.0.1:1234/v1/");
-    auto chat = openai::chat().create(R"(
+    std::string chatresponse="";
+    auto  chat = openai::chat().create(R"(
         {
             "model": "deepseek-r1-distill-qwen-7b",
-            "messages":[{"role":"user", "content":"play game"}],
-            "max_tokens": 1000,
-            "temperature": 0
+            "messages":[{"role":"user", "content":"i want to play as a oliwier the human"}],
+            "max_tokens": 2000,
+            "temperature": 0,
+            "id": "chatcmpl-aye2e09u94leibtysrwob"
         }
         )"_json);
-        std::cout << "Response is:\n" << chat.dump(2) << '\n'; 
-    std::cout << "hello from main.cpp";
- 
+        
+    chatresponse= chat["choices"][0]["message"]["content"].dump();
+    size_t start_conv = chatresponse.rfind("</think>");
+    chatresponse = chatresponse.substr(start_conv + std::string("</think>").length());
+    std::cout << "response :" << chatresponse << std::endl;
     return 0;
 }
