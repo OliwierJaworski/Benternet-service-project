b99c91d8439c92b36543b8e944ce104a455f7148
Oliwier_linux <oliwier.jaworski@student.pxl.be>
2025-04-08 20:49:47 +0200

    #7 user can interact with ai via zmq.hpp

--- app/server/server.cpp
+++ app/server/server.cpp
@@ -1,31 +1,43 @@
-#include <iostream>
 #include <string>
 #include <chrono>
 #include <thread>
+#include <iostream>
 
 #include <zmq.hpp>
+#include <openai.hpp>
+
+using namespace nlohmann;
 
 int main() 
 {
     using namespace std::chrono_literals;
 
-    std::string modelPath = "../../gemma-3-1b-it-Q2_K.gguf";
-    float temperature = 1.0f;
-    float minP = 0.05f;
-    std::unique_ptr<LLMInference> llmInference = std::make_unique<LLMInference>();
-    llmInference->loadModel(modelPath, minP, temperature);
-
-    llmInference->addChatMessage("You are a helpful assistant", "system");
-
     // initialize the zmq context with a single IO thread
     zmq::context_t context{1};
 
+    openai::start("lm-studio","",true,"http://127.0.0.1:1234/v1/");
+    std::string chatresponse="";
+
+    json uquery = R"(
+        {
+            "model": "deepseek-r1-distill-qwen-7b",
+            "messages":[{"role":"user", "content":" "}],
+            "max_tokens": 2000,
+            "temperature": 0
+        }
+        )"_json;
+
     // construct a REP (reply) socket and bind to interface
     zmq::socket_t socket{context, zmq::socket_type::rep};
-    socket.bind("tcp://*:5555");
 
-    std::string predicted_message ="";
-    std::cout << "initialized socket etc..." << std::endl;
+    socket.bind("tcp://*:5555");
+    
+
+
+
+    // prepare some static data for responses
+    const std::string data{"World"};
+
     for (;;) 
     {
         zmq::message_t request;
@@ -33,18 +45,19 @@ int main()
         // receive a request from client
         socket.recv(request, zmq::recv_flags::none);
         std::cout << "Received " << request.to_string() << std::endl;
-       
-        llmInference->startCompletion(request.to_string());
 
-        std::string predictedToken;
-        while ((predictedToken = llmInference->completionLoop()) != "[EOG]") {
-            predicted_message +=predictedToken;
-        }
-        // send the reply to the client
-        socket.send(zmq::buffer(predicted_message), zmq::send_flags::none);
-        predicted_message.clear();
+        uquery["messages"].push_back({{"role","user"},{"content",request.to_string()}});
+
+        std::cout << "Received " << request.to_string() << std::endl;
+
+        auto chat = openai::chat().create(uquery);
+
+        chatresponse= chat["choices"][0]["message"]["content"].dump();
+        size_t start_conv = chatresponse.rfind("</think>");
+        chatresponse = chatresponse.substr(start_conv + std::string("</think>").length());
+
+        socket.send(zmq::buffer(chatresponse) , zmq::send_flags::none);
     }
 
     return 0;
 }
-
